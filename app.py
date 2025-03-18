13e3fbf2ba1451b58496f8ad5ad27941b490d2aa
Tomas Garcia <122606075+tgarciar@users.noreply.github.com>
2025-03-18 16:04:56 +0000

    Well done!

--- app.py
+++ app.py
@@ -15,9 +15,9 @@ This is text
 
 ## This is a list:
 ---
-- Hello
-- This
-- Is
+- Tomi
+- Was
+- Here
 - A
 - List
 
@@ -66,15 +66,7 @@ def get_plotly_data():
     z = z_data.values
     sh_0, sh_1 = z.shape
     x, y = np.linspace(0, 1, sh_0), np.linspace(0, 1, sh_1)
-    return x, y, z
-
-import plotly.graph_objects as go
-
-x, y, z = get_plotly_data()
-
-fig = go.Figure(data=[go.Surface(z=z, x=x, y=y)])
-fig.update_layout(title='IRR', autosize=False, width=800, height=800, margin=dict(l=40, r=40, b=40, t=40))
-st.plotly_chart(fig)
+    return x, y ,z
 
 st.markdown("""
             ---
